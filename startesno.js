09cab29af6461511774acb4ad17b7d11a75c89c1
hh <huihui27@126.com>
2023-03-22 03:48:34 +0800

    12

--- startesno.js
+++ startesno.js
@@ -1,1 +1,6 @@
-require('esno')('./src/index.ts');
+import esno from 'esno';
+import path from 'path';
+
+esno(path.join(process.cwd(), './src/index.ts')).then((mainModule) => {
+  mainModule.default();
+});
