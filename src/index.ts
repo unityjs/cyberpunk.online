db0c52c944a3185a746ab94b0e2b184d1cb0a4ab
hh <huihui27@126.com>
2023-03-21 21:45:15 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function main() {
     initRelayServer(server)
   }else{
     const server = createServer(app)
-    const port = process.env.PORT || 5001
+    const port = process.env.PORT || 5000
     server.listen(port, function () { console.log(`Running on ${port}`) })
     initRelayServer(server)
   }
